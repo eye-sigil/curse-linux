789c3de9ba0d18bda00c7d9ca8b2c01f9bf86ab0
eye-sigil <ustin.a.h@gmail.com>
2016-05-01 03:45:50 -0400

    Update to workign version.

--- main.js
+++ main.js
@@ -21,17 +21,24 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
 	//Loading Screen
-	loadWindow = new BrowserWindow({width: 800,
-		height: 480,
-		icon: __dirname + '/icon.png',
-		title: "Curse",
-		'autoHideMenuBar': true,
-		frame: false});
-	loadWindow.setResizable(false);
-	loadWindow.loadURL('file://' + __dirname + '/load.html');
+	// loadWindow = new BrowserWindow({width: 800,
+	// 	height: 480,
+	// 	icon: __dirname + '/icon.png',
+	// 	title: "Curse",
+	// 	'autoHideMenuBar': true,
+	// 	frame: false});
+	// loadWindow.setResizable(false);
+	// loadWindow.loadURL('file://' + __dirname + '/load.html');
 
-	//mainWindow.on('closed', function() {
-		//mainWindow = null;
-	//});
+	mainWindow = new BrowserWindow({width: 900,
+		height: 750,
+		icon: __dirname + '/icon.png',
+		title: 'Curse',
+		'autoHideMenuBar': true});
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+	mainWindow.on('closed', function() {
+		mainWindow = null;
+	});
 
 });
