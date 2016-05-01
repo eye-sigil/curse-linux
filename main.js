68349384d4c0dbcb0ae454ac7bcd7ffb7a300872
eye-sigil <ustin.a.h@gmail.com>
2016-05-01 18:45:39 -0400

    Continued work on trying to get external links to open in browser. If anyone knows, please drop me a message or a push request.

--- main.js
+++ main.js
@@ -5,10 +5,11 @@ const electron = require('electron');
 const app = require('app');
 const shell = require('shell');
 const BrowserWindow = require('browser-window')
-// const run = require('child_process');
+const run = require('child_process');
 
 //var loadWindow = null;
 var mainWindow = null;
+var mainPage = __dirname + 'index.html';
 //var connected = false;
 
 // openBrowser(url) = {
@@ -33,18 +34,23 @@ app.on('ready', function() {
 	// loadWindow.setResizable(false);
 	// loadWindow.loadURL('file://' + __dirname + '/load.html');
 
-	mainWindow = new BrowserWindow({width: 1000,
+	mainWindow = new BrowserWindow({width: 1100,
 		height: 750,
 		icon: __dirname + '/icon.png',
 		title: 'Curse',
 		'autoHideMenuBar': true});
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
+	// document.on('click', 'a[href^="http"]', function(event) {
+	// 	event.preventDefault();
+	// 	shell.openExternal(this.href);
+	// });
+
 	var webContents = mainWindow.webContents;
 
 	webContents.on('will-navigate', function(e,url) {
 		e.preventDefault();
-		shell.openExternal(url);
+		run.execSync('xdg-open ' + url);
 	});
 
 	mainWindow.on('closed', function() {
